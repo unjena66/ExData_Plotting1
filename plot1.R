6959fc08b6b93aaf61f31eb8d486959810b41157
HanJongsoo <hanjongsoo1@HanJongsooui-iMac.local>
2015-11-06 15:40:06 +0900

    course project 1

---
+++ plot1.R
@@ -0,0 +1,23 @@
+# loading the data
+elec <- read.csv("household_power_consumption.txt", header = T, stringsAsFactors = F, sep = ";")
+
+# paste the date and time
+elec$DateTime<-paste(elec$Date, elec$Time)
+elec_paste<-elec[,-c(1,2)]
+
+# then convert the datetime with strptime() function.
+elec_paste$DateTime <- strptime(elec_paste$DateTime, "%d/%m/%Y %H:%M:%S")
+
+# using data from the dates 2007-02-01 and 2007-02-02
+electric <- elec_paste[format(elec_paste$DateTime, "%Y-%m-%d") %in% c('2007-02-01', '2007-02-02'),]
+
+# create PNG
+png("plot1.png", width = 480, height = 480)
+
+electric$Global_active_power <- as.numeric(electric$Global_active_power)
+
+# create plot
+with(electric, hist(Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power"))
+
+# off
+dev.off()
